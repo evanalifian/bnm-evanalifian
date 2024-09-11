30a04bee5fa92e628710227acd251422858985b3
evanalifian <evanalifian@gmal.com>
2024-09-11 11:31:22 +0700

    initial init

---
+++ public/js/script.js
@@ -0,0 +1,32 @@
+function getData() {
+  const data = fetch('../../data.json').then(res => res.json())
+  return data
+}
+
+
+async function friendListsElement() {
+  const friendLists = document.getElementById("friend_list");
+  const data = await getData()
+  const lists = `
+    ${data.map(i => `
+      <li>
+        <figure class="card">
+          <div class="card_image">
+            <img src="${i.fotoselfie}" alt="${i.nama}" width="100%">
+          </div>
+          <figcaption class="card_content">
+            <span>Nama : ${i.nama}</span>
+            <span>NIM : ${parseInt(i.nim)}</span>
+            <span>TTL : ${i.ttl}</span>
+            <span>Alamat : ${i.alamat}</span>
+            <span>NoHP : ${i.no}</span>
+          </figcaption>
+        </figure>
+      </li>
+    `).join("")}
+  `;
+
+  return friendLists.innerHTML = lists
+}
+
+friendListsElement()
